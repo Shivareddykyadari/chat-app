9a778d96a47a22584d8cd5f6a25b95988428e658
Kyadari Shiva Kumar Reddy <101393864+Shivareddykyadari@users.noreply.github.com>
2024-11-23 16:54:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './App/Login';
+import Chat from './App/Chat';
+import Registration from './App/Registration';
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="chat"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="chat" component={Chat} />
+        <Stack.Screen name="registrtation" component={Registration} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
